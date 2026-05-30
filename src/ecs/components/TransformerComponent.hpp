c3282def0ef3cb154ccfb5f29f56ccc47afda33d
Itziar <itzimr97@pm.me>
2026-05-30 23:47:46 +0200

    feat: added transform component

---
+++ src/ecs/components/TransformerComponent.hpp
@@ -0,0 +1,17 @@
+// src/etc/components/TransformerComponent.hpp
+#pragma once
+#include "../Component.hpp"
+#include <glm/vec2.hpp>
+
+struct TransformComponent : Component {
+  glm::vec2 position{0.0f, 0.0f};
+  glm::vec2 scale{1.0f, 1.0f};
+  float rotation{0.0f}; // degrees
+
+  TransformComponent() = default;
+  TransformComponent(glm::vec2 pos, glm::vec2 scl = {1, 1}, float rot = 0.0f)
+      : position(pos), scale(scl), rotation(rot) {}
+
+  void Update(float) override {} // transform is data-only
+  void Render() override {}
+};
