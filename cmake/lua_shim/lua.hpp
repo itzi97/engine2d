327d22d4ec788ef597484fa193c5e393e3854e79
Itziar <itzimr97@pm.me>
2026-05-30 23:39:36 +0200

    bugfix: added de-mangling to the library

--- cmake/lua_shim/lua.hpp
+++ cmake/lua_shim/lua.hpp
@@ -1,6 +1,8 @@
 // Shim: redirect <lua.hpp> to the fetched Lua 5.4 headers.
 // This file must live in a directory that appears before /usr/include
 // on the compiler search path so it shadows the system lua.hpp.
+extern "C" {
+#include "lauxlib.h"
 #include "lua.h"
 #include "lualib.h"
-#include "lauxlib.h"
+}
