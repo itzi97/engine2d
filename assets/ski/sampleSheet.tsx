5bd41ea44a8737099373e2705d5b6db7453123a8
Itziar <56316427+itzi97@users.noreply.github.com>
2026-06-04 14:34:27 +0200

    assets: fix tileset and map source paths

--- assets/ski/sampleSheet.tsx
+++ assets/ski/sampleSheet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="tilemap" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
- <image source="../Tilemap/tilemap.png" width="203" height="186"/>
+ <image source="tilemap_packed.png" width="193" height="176"/>
 </tileset>
